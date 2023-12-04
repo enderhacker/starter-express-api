61d5211c06b67045094129b5076f0aa751133691
enderhacker <61245488+enderhacker@users.noreply.github.com>
2023-12-04 17:12:22 +0100

    Update index.js

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ const port = 3000; // Change as needed
 // Endpoint to reset the table
 app.get('/version', async (req, res) => {
     const queryText = req.query.text;
-    owospeak(queryText, { stutter: true, tilde: true });
-    res.status(200).json({ success: true, message: 'Ayoooooo bro said what?!' });
+    const result = await owospeak(queryText, { stutter: true, tilde: true });
+    res.status(200).json({ success: true, message: result });
 });
 
 app.listen(port, () => {
