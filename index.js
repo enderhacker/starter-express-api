2a1628ed9673c23559a4d2b4b372fddc28e14b8d
enderhacker <61245488+enderhacker@users.noreply.github.com>
2023-12-04 17:08:06 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,23 +3,8 @@ const express = require('express');
 const app = express();
 const port = 3000; // Change as needed
 
-app.get('/api/getTableData', async (req, res) => {
-  try {
-    // Retrieve all items from the database
-    const allItems = await tableDataCollection.getAll();
-
-    // Convert the items to an array
-    const tableData = Object.values(allItems);
-
-    res.json(tableData);
-  } catch (error) {
-    console.error('Error retrieving table data:', error.message);
-    res.status(500).json({ success: false, message: 'Internal Server Error' });
-  }
-});
-
 // Endpoint to reset the table
-app.post('/version', async (req, res) => {
+app.get('/version', async (req, res) => {
     res.status(200).json({ success: true, message: 'Ayoooooo bro said what?!' });
 });
 
